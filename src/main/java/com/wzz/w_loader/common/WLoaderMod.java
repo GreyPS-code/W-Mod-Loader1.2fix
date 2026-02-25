55204794c836a61de811a70f6448f3c3225d7b63
GreyPS-code <wyz2013158@163.com>
2026-02-25 16:41:28 +0800

    Remove event handlers from WLoaderMod
    Removed event handling methods that cancel events or modify health.

--- src/main/java/com/wzz/w_loader/common/WLoaderMod.java
+++ src/main/java/com/wzz/w_loader/common/WLoaderMod.java
@@ -9,28 +9,5 @@ import net.minecraft.world.level.GameType;
 
 @WMod(name = "W Loader", modId = "w_loader")
 public class WLoaderMod {
-    @Subscribe
-    public void onLivingHurtEq(LivingHurtEquipmentEvent event) {
-        event.setCancelled(true);
-    }
-
-    @Subscribe
-    public void onK(LivingKnockBackEvent event) {
-        event.setCancelled(true);
-    }
-
-    @Subscribe
-    public void onE(LivingDropEvent event) {
-        event.setCancelled(true);
-    }
-
-    @Subscribe
-    public void onAD(LivingGetHealthEvent event) {
-        event.setValue(20f);
-    }
-
-    @Subscribe
-    public void onS(LivingStartUsingItemEvent event) {
-        event.setCancelled(true);
-    }
+    //donothing
 }
